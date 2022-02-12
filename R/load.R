7605a1496521a977851b6e5c3b120005f6eae642
David Widmann <devmotion@users.noreply.github.com>
2022-02-12 02:37:58 +0100

    Update load.R

--- R/load.R
+++ R/load.R
@@ -1,45 +1,19 @@
 #' Load namespace of CalibrationErrors.jl and CalibrationTests.jl
 #'
 #' @export
-load <- function() wrap_julia_pkgs(c("CalibrationErrors", "CalibrationTests"))
+load <- function() {
+  # Define RCalibration submodule
+  JuliaCall::julia_command(paste0('include("', system.file("julia/RCalibration.jl", package = "rcalibration"), '")'))
 
-wrap_julia_pkgs <- function(pkg_list) {
-  exports <- unique(unlist(lapply(pkg_list, julia_exports)))
-  julia_pkgs_import(pkg_list, func_list = exports)
+  # Obtain exports of CalibrationErrors and CalibrationTests
+  mods <- c("RCalibration.CalibrationErrors", "RCalibration.CalibrationTests")
+  exports <- unique(unlist(lapply(mods, julia_exports)))
+
+  JuliaCall::julia_pkg_import("RCalibration", exports)
 }
 
-julia_exports <- function(pkg) {
-  # Precompile and load Julia package
-  JuliaCall::julia_library(pkg)
-
-  # Obtain a list of exported symbols with valid identifiers
-  cmd <- paste0("filter(isascii, map(x -> replace(string(x), \"!\" => \"_bang\"), propertynames(", pkg, ")))")
-  exports <- JuliaCall::julia_eval(cmd)
-
-  exports
-}
-
-# Copied from JuliaCall and extended to multiple packages
-julia_pkgs_import <- function(pkg_list, func_list,
-                             env = new.env(parent = emptyenv())){
-  env$setup <- function(...){
-    JuliaCall::julia_setup(...)
-    for (pkg in pkg_list) {
-      JuliaCall::julia_library(pkg)
-    }
-    for (hook in env$hooks) {
-      if (is.function(hook)) {hook()}
-      else {warning("Some hook is not a function.")}
-    }
-    env$initialized <- TRUE
-  }
-  for (fname in func_list) {
-    JuliaCall::julia_function(func_name = fname,
-                   pkg_name = pkg_name,
-                   env = env)
-  }
-  reg.finalizer(env,
-                function(e) e$initialized <- FALSE,
-                onexit = TRUE)
-  env
+#' Obtain a list of exported symbols with valid identifiers
+julia_exports <- function(mod) {
+  cmd <- paste0("filter(isascii, map(x -> replace(string(x), \"!\" => \"_bang\"), propertynames(", mod, ")))")
+  JuliaCall::julia_eval(cmd)
 }
